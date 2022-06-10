6c86e6d8b39a46b4d7de36c0fe2f153b339c0146
Austin Imbastari <fvckimba@Austins-MBP.lan>
2022-06-10 12:09:34 -0700

    generated colors

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 "use strict";
+//global selection & variables
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHex = document.querySelectorAll(".color h2");
+let initialColors;
+//function
+function generateHex() {
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+function randomColors() {
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0];
+        console.log(hexText);
+        const randomColors = generateHex();
+
+        //add to bacground
+        div.style.backgroundColor = randomColors;
+        hexText.innerText = randomColors;
+    });
+}
+
+randomColors();
