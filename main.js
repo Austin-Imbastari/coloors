8d17b7d515b0d47cfa8de1d6dda79d3c005bfce0
Austin Imbastari <fvckimba@Austins-MBP.lan>
2022-06-10 13:21:41 -0700

    input scale slider background

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHex = document.querySelectorAll(".color h2");
 let initialColors;
+
+//event listeners
+sliders.forEach((slider) => {
+    slider.addEventListener("input", hslControls);
+});
+
 //function
 function generateHex() {
     const hexColor = chroma.random();
@@ -14,7 +20,6 @@ function generateHex() {
 function randomColors() {
     colorDivs.forEach((div, index) => {
         const hexText = div.children[0];
-        console.log(hexText);
         const randomColors = generateHex();
 
         //add to bacground
@@ -27,7 +32,6 @@ function randomColors() {
         const color = chroma(randomColors);
         const sliders = div.querySelectorAll(".sliders input");
         const hue = sliders[0];
-        console.log(hue);
         const brightness = sliders[1];
         const saturation = sliders[2];
 
@@ -60,6 +64,29 @@ function colorizeSliders(color, hue, brightness, saturation) {
     )}, ${scaleBright(0.5)},${scaleBright(1)})`;
 
     hue.style.backgroundImage = `linear-gradient(to right, rgb(204,74,75), rgb(204,204,75), rgb(74,204,75),rgb(75,204,204),rgb(74,75,204), rgb(204,75,204),rgb(204,75,75))`;
+}
+
+function hslControls(e) {
+    const index =
+        e.target.getAttribute("data-bright") ||
+        e.target.getAttribute("data-sat") ||
+        e.target.getAttribute("data-hue");
+    let sliders = e.target.parentElement.querySelectorAll(
+        "input[type='range']"
+    );
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    const bgColor = colorDivs[index].querySelector("h2").innerText;
+    console.log(bgColor);
+
+    let color = chroma(bgColor)
+        .set("hsl.s", saturation.value)
+        .set("hsl.l", brightness.value)
+        .set("hsl.h", hue.value);
+
+    colorDivs[index].style.backgroundColor = color;
 }
 
 randomColors();
