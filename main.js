91b609d116600c5e060af2c6a1a70674df2a8f11
Austin Imbastari <fvckimba@Austins-MBP.lan>
2022-06-10 12:43:54 -0700

    input scale color added

--- main.js
+++ main.js
@@ -20,7 +20,46 @@ function randomColors() {
         //add to bacground
         div.style.backgroundColor = randomColors;
         hexText.innerText = randomColors;
+        // check for contrast
+        checkTextContrast(randomColors, hexText);
+
+        //initial colorize sliders
+        const color = chroma(randomColors);
+        const sliders = div.querySelectorAll(".sliders input");
+        const hue = sliders[0];
+        console.log(hue);
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
     });
+}
+
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    luminance > 0.5
+        ? (text.style.color = "black")
+        : (text.style.color = "white");
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+    const noSat = color.set("hsl.s", 0);
+    const fullSat = color.set("hsl.s", 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+    //input update colors
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(
+        0
+    )}, ${scaleSat(1)})`;
+
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(
+        0
+    )}, ${scaleBright(0.5)},${scaleBright(1)})`;
+
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204,74,75), rgb(204,204,75), rgb(74,204,75),rgb(75,204,204),rgb(74,75,204), rgb(204,75,204),rgb(204,75,75))`;
 }
 
 randomColors();
